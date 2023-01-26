6fb306d67cacde245681a34614596218bb9f26d4
Antonino Musmeci <nino.musmeci@gmail.com>
2023-01-26 21:25:52 +0100

    feat: add title and image to card component

--- Frontend/src/components/layout/Content.tsx
+++ Frontend/src/components/layout/Content.tsx
@@ -1,30 +1,32 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import Urlcard from '../Urlcard'
-
+import Urlcard from "../Urlcard";
+import { UrlInfo } from "./types/type";
 
 function Content() {
-  const urlList = [0,1,2,3,4,5,6,7]
-  const baseURL = "http://localhost:3001/newspaper/?url=https://www.freecodecamp.org/news/how-to-use-axios-with-react/"
-  
-  const [urlInfo, setUrlInfo] = useState()
+  const urlList = [0, 1, 2, 3, 4, 5, 6, 7];
+  const baseURL =
+    "http://localhost:3001/newspaper/?url=https://www.freecodecamp.org/news/how-to-use-axios-with-react/";
+
+  const [urlInfo, setUrlInfo] = useState<UrlInfo>();
   useEffect(() => {
-    axios.get(baseURL).then((response) => {
+    axios.get<UrlInfo>(baseURL).then((response) => {
       setUrlInfo(response.data);
     });
-  }, []) 
+  }, []);
 
-  useEffect(() => {console.log(urlInfo)},[urlInfo])
+  useEffect(() => {
+    console.log(urlInfo);
+  }, [urlInfo]);
   return (
-    <div className='content'>
-        <div className="content__cards-wrapper">
-        {urlList.map(() => 
-            <Urlcard />
-        )}
-        </div>
-
+    <div className="content">
+      <div className="content__cards-wrapper">
+        {urlList.map(() => (
+          <Urlcard title = {urlInfo?.title || ""} image = {urlInfo?.images || ""} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Content
+export default Content;
--- Frontend/src/components/layout/types/type.ts
+++ Frontend/src/components/layout/types/type.ts
@@ -4,3 +4,11 @@ export type HeaderProps = {
     showSidebar: boolean;
     setShowSidebar: React.Dispatch<SetStateAction<boolean>>
   }
+
+export type UrlInfo = {
+  publicationDate: string;
+  title: string;
+  images: string;
+  authors: string[];
+  keywords: string[];
+}
