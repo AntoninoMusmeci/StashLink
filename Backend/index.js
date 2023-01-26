c65347f5ec10342538f7b80322efecc4c5d277df
Antonino Musmeci <nino.musmeci@gmail.com>
2023-01-26 21:07:15 +0100

    feat: add cors

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const getWebPage = require("./controller.js");
+const cors = require('cors');
 const app = express();
 const port = 3001;
+app.use(cors());
 
 app.get("/newspaper", async (req, res) => {
   const url = req.query.url
