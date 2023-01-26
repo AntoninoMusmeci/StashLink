de6325f2dce0ef8554c91614adddae1d80cf8bbe
Antonino Musmeci <nino.musmeci@gmail.com>
2023-01-26 21:08:20 +0100

    test: fetch url data from api using axios

--- Frontend/src/components/layout/Content.tsx
+++ Frontend/src/components/layout/Content.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from "axios";
 import Urlcard from '../Urlcard'
 
-const urlList = [0,1,2,3,4,5,6,7]
+
 function Content() {
+  const urlList = [0,1,2,3,4,5,6,7]
+  const baseURL = "http://localhost:3001/newspaper/?url=https://www.freecodecamp.org/news/how-to-use-axios-with-react/"
+  
+  const [urlInfo, setUrlInfo] = useState()
+  useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setUrlInfo(response.data);
+    });
+  }, []) 
+
+  useEffect(() => {console.log(urlInfo)},[urlInfo])
   return (
     <div className='content'>
         <div className="content__cards-wrapper">
